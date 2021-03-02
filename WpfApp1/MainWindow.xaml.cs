26688db475206f7b9cc29df52183f8403c56d390
Aline ELLUL <aline.ellul@gmail.com>
2021-03-02 08:58:50 +0100

    Ajoutez des fichiers projet.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {// string filename = @"C:\Users\aellul\Desktop\WpfApp1\WpfApp1\bin\Debug\coco.bmp";
+        OpenFileDialog dlg;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.bmp)|*.bmp";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = null;
+            
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = dlg.FileName;
+                                     
+            }
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(filename);         
+            bitmap.EndInit();
+            ImageViewer.Source = bitmap;
+        }
+            }
+        }
+    
+
