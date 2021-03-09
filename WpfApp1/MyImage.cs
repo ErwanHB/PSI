f1433c1a97529807221b849842d9edaff16adf63
GuillaumeBG <guillaume.bourg24@gmail.com>
2021-03-09 16:50:17 +0100

    correction bourrage

--- WpfApp1/MyImage.cs
+++ WpfApp1/MyImage.cs
@@ -44,7 +44,7 @@ namespace WpfApp1
         int taille;
         int offset;
         int largeur;
-        int longueur;
+        int hauteur;
         int nombreBitsParPixel;
         Pixel[,] matriceBGR;
         byte[] header;
@@ -53,9 +53,9 @@ namespace WpfApp1
         {
             get { return this.largeur; }
         }
-        public int Longueur
+        public int Hauteur
         {
-            get { return this.longueur; }
+            get { return this.hauteur; }
         }
         public byte[] Header
         {
@@ -82,11 +82,11 @@ namespace WpfApp1
 
                 this.largeur = Convertir_Endian_To_Int(myfile, 18, 4);
 
-                this.longueur = Convertir_Endian_To_Int(myfile, 22, 4);
+                this.hauteur = Convertir_Endian_To_Int(myfile, 22, 4);
 
                 this.nombreBitsParPixel = Convertir_Endian_To_Int(myfile, 28, 2);
 
-                this.matriceBGR = new Pixel[this.longueur, this.largeur];
+                this.matriceBGR = new Pixel[this.hauteur, this.largeur];
 
                 this.header = new byte[this.offset];
 
@@ -97,8 +97,10 @@ namespace WpfApp1
 
                 int cpt = this.offset;
                 int[] rvb = new int[3];
+                int bourrage = (this.largeur * this.nombreBitsParPixel/8) % 4;
+                if (bourrage != 0) bourrage = 4 - bourrage;
 
-                for (int i = 0; i < this.longueur; i++)
+                for (int i = 0; i < this.hauteur; i++)
                 {
                     for (int j = 0; j < this.largeur; j++)
                     {
@@ -110,9 +112,9 @@ namespace WpfApp1
                         Pixel temp = new Pixel(rvb);
                         this.matriceBGR[i, j] = temp;
                     }
-                    if ((this.longueur * 3) % 4 != 0)
+                    if (bourrage != 0)
                     {
-                        cpt += (this.longueur * 3) % 4;
+                        cpt += bourrage;
                     }
                 }
             }
@@ -135,14 +137,14 @@ namespace WpfApp1
 
                 this.largeur = Convertir_Endian_To_Int(header, 18, 4);
 
-                this.longueur = Convertir_Endian_To_Int(header, 22, 4);
+                this.hauteur = Convertir_Endian_To_Int(header, 22, 4);
 
-                if (this.largeur % 4 == 0 && this.longueur % 4 == 0)
+                if (this.largeur % 4 == 0 && this.hauteur % 4 == 0)
                 {
                     this.nombreBitsParPixel = Convertir_Endian_To_Int(header, 28, 2);
                 }
             }
-            this.matriceBGR = new Pixel[this.largeur, this.longueur];
+            this.matriceBGR = new Pixel[this.largeur, this.hauteur];
         }
 
         /// <summary>
@@ -163,9 +165,9 @@ namespace WpfApp1
 
                 this.largeur = Convertir_Endian_To_Int(header, 18, 4);
 
-                this.longueur = Convertir_Endian_To_Int(header, 22, 4);
+                this.hauteur = Convertir_Endian_To_Int(header, 22, 4);
 
-                if (this.largeur % 4 == 0 && this.longueur % 4 == 0)
+                if (this.largeur % 4 == 0 && this.hauteur % 4 == 0)
                 {
                     this.nombreBitsParPixel = Convertir_Endian_To_Int(header, 28, 2);
                 }
@@ -183,8 +185,10 @@ namespace WpfApp1
         public void From_Image_To_File(string file)
         {
             byte[] bytes = new byte[this.taille];
-            int index = 0;
-            int bourrage = (this.matriceBGR.GetLength(1) * 3) % 4;
+            uint index = 0;
+            int bourrage = (this.largeur * this.nombreBitsParPixel/8) % 4;
+            if (bourrage != 0) bourrage = 4 - bourrage;
+
             for (int i = 0; i < this.offset; i++)
             {
                 bytes[index] = this.header[i];
@@ -266,222 +270,6 @@ namespace WpfApp1
         }
 
         #endregion
-
-        /*
-        #region Instance de la classe MyImage
-        string type;
-        int taille;
-        int offset;
-        int largeur;
-        int longueur;
-        int nombreBitsParPixel;
-        Pixel[,] matriceBGR;
-        byte[] header;
-
-        public int Largeur
-        {
-            get { return this.largeur; }
-        }
-        public int Longueur
-        {
-            get { return this.longueur; }
-        }
-        public byte[] Header
-        {
-            get { return this.header; }
-        }
-        #endregion
-
-        #region Constructeur de la classe MyImage
-        /// <summary>
-        /// Constructeur de la classe MyImage qui lit un fichier ( .bmp).
-        /// </summary>
-        /// <param name="image">Nom de l'image</param>
-        public MyImage(string image)
-        {
-            byte[] myfile = File.ReadAllBytes(image);
-
-            if (myfile[0] == 66 && myfile[1] == 77)
-            {
-                this.type = "BM";
-
-                this.taille = Convertir_Endian_To_Int(myfile, 2, 4);
-
-                this.offset = Convertir_Endian_To_Int(myfile, 10, 4);
-
-                this.largeur = Convertir_Endian_To_Int(myfile, 18, 4);
-
-                this.longueur = Convertir_Endian_To_Int(myfile, 22, 4);
-
-                if (this.largeur % 4 == 0 && this.longueur % 4 == 0)
-                {
-                    this.nombreBitsParPixel = Convertir_Endian_To_Int(myfile, 28, 2);
-
-                    this.matriceBGR = new Pixel[this.longueur, this.largeur];
-
-                    this.header = new byte[this.offset];
-
-                    for (int i = 0; i < this.offset; i++)
-                    {
-                        this.header[i] = myfile[i];
-                    }
-
-                    int index1 = 0;     // premier parametre de la marice
-                    int index2 = 0;     // premier parametre de la marice
-                    int cpt = 0;
-                    for (int i = this.offset; i < this.taille; i += 3)
-                    {
-                        index2 = cpt % this.largeur;
-                        index1 = cpt / this.largeur;
-                        Pixel temp = new Pixel(myfile[i + 2], myfile[i + 1], myfile[i]);
-                        this.matriceBGR[index1, index2] = temp;
-                        cpt++;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Constructeur de la classe Image à partir d'un header seulement
-        /// </summary>
-        /// <param name="header"></param>
-        public MyImage(byte[] header)
-        {
-            this.header = header;
-            if (header[0] == 66 && header[1] == 77)
-            {
-                this.type = "BM";
-
-                this.taille = Convertir_Endian_To_Int(header, 2, 4);
-
-                this.offset = Convertir_Endian_To_Int(header, 10, 4);
-
-                this.largeur = Convertir_Endian_To_Int(header, 18, 4);
-
-                this.longueur = Convertir_Endian_To_Int(header, 22, 4);
-
-                if (this.largeur % 4 == 0 && this.longueur % 4 == 0)
-                {
-                    this.nombreBitsParPixel = Convertir_Endian_To_Int(header, 28, 2);
-                }
-            }
-            this.matriceBGR = new Pixel[this.largeur, this.longueur];
-        }
-
-        /// <summary>
-        /// Constructeur de la classe Image à partir d'un header et d'une matrice de pixels RGB
-        /// </summary>
-        /// <param name="header"></param>
-        /// <param name="matriceBGR"></param>
-        public MyImage(byte[] header, Pixel[,] matriceBGR)
-        {
-            this.header = header;
-            if (header[0] == 66 && header[1] == 77)
-            {
-                this.type = "BM";
-
-                this.taille = Convertir_Endian_To_Int(header, 2, 4);
-
-                this.offset = Convertir_Endian_To_Int(header, 10, 4);
-
-                this.largeur = Convertir_Endian_To_Int(header, 18, 4);
-
-                this.longueur = Convertir_Endian_To_Int(header, 22, 4);
-
-                if (this.largeur % 4 == 0 && this.longueur % 4 == 0)
-                {
-                    this.nombreBitsParPixel = Convertir_Endian_To_Int(header, 28, 2);
-                }
-            }
-            this.matriceBGR = matriceBGR;
-        }
-
-        #endregion
-
-        #region Méthode de la classe MyImage
-        /// <summary>
-        /// prend une instance de MyImage et la transforme en fichier binaire respectant la structure du fichier.bmp
-        /// </summary>
-        /// <param name="file">Emplacement du fichier en sortie</param>
-        public void From_Image_To_File(string file)
-        {
-            byte[] bytes = new byte[this.taille];
-            int index = 0;
-            for (int i = 0; i < this.offset; i++)
-            {
-                bytes[index] = this.header[i];
-                index++;
-            }
-            for (int i = 0; i < this.matriceBGR.GetLength(0); i++)
-            {
-                for (int j = 0; j < this.matriceBGR.GetLength(1); j++)
-                {
-                    for (int a = 0; a < 3; a++)
-                    {
-                        bytes[index] = Convert.ToByte(matriceBGR[i, j].B);
-                        bytes[index] = Convert.ToByte(matriceBGR[i, j].V);
-                        bytes[index] = Convert.ToByte(matriceBGR[i, j].R);
-                        index++;
-                    }
-                }
-            }
-        File.WriteAllBytes(file, bytes); //./Images/Sortie.bmp
-        }
-
-        /// <summary>
-        /// Convertit une séquence d’octets au format little endian en entier.
-        /// </summary>
-        /// <param name="tab">tableau à convertir</param>
-        /// <param name="debut">place dans le tableau,de 1 à..., du début de la sequence à decoder </param>
-        /// <param name="nombre">nombre d'octect à décoder</param>
-        /// <returns></returns>
-        public int Convertir_Endian_To_Int(byte[] tab, int debut, int nombre)
-        {
-            double somme = 0;
-            int cpt = 0;
-            for (int i = debut; i < debut + nombre; i++)
-            {
-                somme += tab[i] * Math.Pow(256, cpt);
-                cpt++;
-            }
-            return Convert.ToInt32(somme);
-        }
-
-        /// <summary>
-        /// convertit un entier en séquence d’octets au format little endian.
-        /// </summary>
-        /// <param name="val">valeur à convertir</param>
-        /// <param name="taille">nombre d'octect sur lequel transcrire la valeur</param>
-        /// <returns></returns>
-        public byte[] Convertir_Int_To_Endian(int val, int taille)
-        {
-            int test = 0;
-            for (int i = 1; i <= taille; i++)
-            {
-                test += Convert.ToInt32(Math.Pow(256, i));
-            }
-            if (test < val) return null;
-            else
-            {
-                byte[] tab = new byte[taille];
-                while (val / 256 != 0)
-                {
-                    taille--;
-                    tab[taille] = Convert.ToByte(val / Convert.ToInt32(Math.Pow(256, taille)));
-                    val -= val % Convert.ToInt32(Math.Pow(256, taille));
-                }
-                return tab;
-            }
-        }
-
-        public Pixel[,] MatriceBGR
-        {
-            get { return this.matriceBGR; }
-            set { this.matriceBGR = value; }
-        }
-
-        #endregion*/
-
         #endregion
     }
 }
